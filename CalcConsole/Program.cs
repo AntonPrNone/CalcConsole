a4185695606b158fb12f863c68c404e6d7dce698
Антон <61877084+AntonPrNone@users.noreply.github.com>
2024-01-20 14:10:43 +0300

    финалочка

--- CalcConsole/Program.cs
+++ CalcConsole/Program.cs
@@ -1,111 +1,210 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CalcConsole
 {
-    internal class Program
+    class Program
     {
         static void Main()
         {
+            PrintOperatorsTable();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            PrintCenteredText("Простой консольный калькулятор");
+
             while (true)
             {
-                double num1, num2, result;
-                char operation;
+                Console.ForegroundColor = ConsoleColor.Blue;
 
-                Console.ForegroundColor = ConsoleColor.Blue; // Цвет сообщений - синий
+                double num1 = GetUserInputAsDouble("Введите первое число:");
+                char operation = GetUserInputAsChar("Выберите оператор:");
 
-                Console.WriteLine("Простой консольный калькулятор");
-                Console.WriteLine("Введите первое число:");
-                num1 = Convert.ToDouble(GetInput(ConsoleColor.White)); // Ввод пользователя белым цветом
-
-                Console.WriteLine("Выберите операцию (+, -, *, /, ^, s (2√), !):");
-                operation = Convert.ToChar(GetInput(ConsoleColor.White)); // Ввод пользователя белым цветом
-
-                switch (operation)
+                double? num2 = null;
+                if (IsBinaryOperation(operation))
                 {
-                    case '+':
-                        Console.WriteLine("Введите второе число:");
-                        num2 = Convert.ToDouble(GetInput(ConsoleColor.White)); // Ввод пользователя белым цветом
-                        result = num1 + num2;
-                        break;
-                    case '-':
-                        Console.WriteLine("Введите второе число:");
-                        num2 = Convert.ToDouble(GetInput(ConsoleColor.White)); // Ввод пользователя белым цветом
-                        result = num1 - num2;
-                        break;
-                    case '*':
-                        Console.WriteLine("Введите второе число:");
-                        num2 = Convert.ToDouble(GetInput(ConsoleColor.White)); // Ввод пользователя белым цветом
-                        result = num1 * num2;
-                        break;
-                    case '/':
-                        Console.WriteLine("Введите второе число:");
-                        num2 = Convert.ToDouble(GetInput(ConsoleColor.White)); // Ввод пользователя белым цветом
-                        if (num2 != 0)
-                        {
-                            result = num1 / num2;
-                        }
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red; // Цвет ошибки - красный
-                            Console.WriteLine("Ошибка: деление на ноль.");
-                            Console.ForegroundColor = ConsoleColor.Blue; // Возвращаем цвет по умолчанию
-                            continue;
-                        }
-                        break;
-                    case '^':
-                        Console.WriteLine("Введите степень:");
-                        num2 = Convert.ToDouble(GetInput(ConsoleColor.White)); // Ввод пользователя белым цветом
-                        result = Math.Pow(num1, num2);
-                        break;
-                    case 's':
-                        result = Math.Sqrt(num1);
-                        break;
-                    case '!':
-                        result = Factorial(num1);
-                        break;
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red; // Цвет ошибки - красный
-                        Console.WriteLine("Ошибка: неверная операция.");
-                        Console.ForegroundColor = ConsoleColor.Blue; // Возвращаем цвет по умолчанию
-                        continue;
+                    num2 = GetUserInputAsDouble("Введите второе число:");
                 }
 
-                Console.ForegroundColor = ConsoleColor.Green; // Цвет ответа - зеленый
-                Console.WriteLine("Результат: " + result);
-                Console.ForegroundColor = ConsoleColor.Blue; // Возвращаем цвет по умолчанию
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Запрос: {num1} {operation} {(num2.HasValue ? num2.Value.ToString() : "")}");
 
-                Console.WriteLine("Нажмите Escape (Esc) для выхода или любую другую клавишу для продолжения.");
+                double result = PerformOperation(num1, num2 ?? double.NaN, operation);
 
-                if (Console.ReadKey().Key == ConsoleKey.Escape)
-                {
-                    break;
-                }
+                Console.WriteLine($"Результат: {result}");
+                Console.ForegroundColor = ConsoleColor.Blue;
+
                 Console.WriteLine();
             }
         }
 
-        static string GetInput(ConsoleColor color)
+        static void PrintCenteredText(string text)
         {
-            Console.ForegroundColor = color;
-            string input = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Blue; // Возвращаем цвет по умолчанию
-            return input;
+            int screenWidth = Console.WindowWidth;
+            int leftPadding = (screenWidth - text.Length) / 2;
+            Console.SetCursorPosition(leftPadding, Console.CursorTop);
+            Console.WriteLine(text);
+        }
+
+        static double GetUserInputAsDouble(string prompt)
+        {
+            double userInput;
+            bool validInput;
+
+            do
+            {
+                Console.Write(prompt + " ");
+                validInput = double.TryParse(Console.ReadLine(), out userInput);
+
+                if (!validInput)
+                {
+                    HandleError("Ошибка: введенное значение не является числом. Пожалуйста, введите корректное число.");
+                }
+            } while (!validInput);
+
+            return userInput;
+        }
+
+        static char GetUserInputAsChar(string prompt)
+        {
+            char userInput;
+            bool validInput;
+
+            do
+            {
+                Console.Write(prompt + " ");
+                validInput = char.TryParse(Console.ReadLine(), out userInput);
+
+                if (!validInput || !IsOperator(userInput))
+                {
+                    HandleError("Ошибка: введенный символ не распознан или не является оператором. Пожалуйста, введите корректный оператор.");
+                    validInput = false;
+                }
+            } while (!validInput);
+
+            return userInput;
+        }
+
+        static bool IsOperator(char symbol)
+        {
+            return symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '^' || symbol == 's' || symbol == '!' || symbol == '%';
+        }
+
+        static bool IsBinaryOperation(char operation)
+        {
+            return operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '^' || operation == '%';
+        }
+
+        static double PerformOperation(double num1, double num2, char operation)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        return num1 / num2;
+                    }
+                    else
+                    {
+                        HandleError("Ошибка: деление на ноль.");
+                        return double.NaN;
+                    }
+                case '%':
+                    if (num2 != 0)
+                    {
+                        return num1 % num2;
+                    }
+                    else
+                    {
+                        HandleError("Ошибка: деление на ноль.");
+                        return double.NaN;
+                    }
+                case '^':
+                    return Math.Pow(num1, num2);
+                case 's':
+                    if (num1 >= 0)
+                    {
+                        return Math.Sqrt(num1);
+                    }
+                    else
+                    {
+                        HandleError("Ошибка: невозможно взять квадратный корень из отрицательного числа.");
+                        return double.NaN;
+                    }
+                case '!':
+                    return Factorial(num1);
+                default:
+                    HandleError("Ошибка: неверная операция.");
+                    return double.NaN;
+            }
+        }
+
+        static void HandleError(string errorMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errorMessage);
+            Console.ForegroundColor = ConsoleColor.Blue;
         }
 
         static double Factorial(double n)
         {
-            if (n == 0)
+            if (n < 0 || n != Math.Floor(n))
             {
-                return 1;
+                HandleError("Ошибка: факториал определен только для неотрицательных целых чисел.");
+                return double.NaN;
             }
-            else
+
+            return (n == 0) ? 1 : n * Factorial(n - 1);
+        }
+
+        static void PrintOperatorsTable()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            int tableWidth = 30;
+            int tableHeight = 11;
+
+            Console.SetCursorPosition(Console.WindowWidth - tableWidth, 0);
+            Console.WriteLine("╔" + new string('═', tableWidth - 2) + "╗");
+
+            for (int i = 1; i < tableHeight - 1; i++)
             {
-                return n * Factorial(n - 1);
+                Console.SetCursorPosition(Console.WindowWidth - tableWidth, i);
+                Console.WriteLine("║" + new string(' ', tableWidth - 2) + "║");
             }
+
+            Console.SetCursorPosition(Console.WindowWidth - tableWidth, tableHeight - 1);
+            Console.WriteLine("╚" + new string('═', tableWidth - 2) + "╝");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(Console.WindowWidth - 25, 1);
+            Console.WriteLine("Операторы:");
+
+            Console.SetCursorPosition(Console.WindowWidth - 25, 2);
+            Console.WriteLine("+ : Сложение");
+
+            Console.SetCursorPosition(Console.WindowWidth - 25, 3);
+            Console.WriteLine("- : Вычитание");
+
+            Console.SetCursorPosition(Console.WindowWidth - 25, 4);
+            Console.WriteLine("* : Умножение");
+
+            Console.SetCursorPosition(Console.WindowWidth - 25, 5);
+            Console.WriteLine("/ : Деление");
+
+            Console.SetCursorPosition(Console.WindowWidth - 25, 6);
+            Console.WriteLine("% : Остаток от деления");
+
+            Console.SetCursorPosition(Console.WindowWidth - 25, 7);
+            Console.WriteLine("^ : Возведение в степень");
+
+            Console.SetCursorPosition(Console.WindowWidth - 25, 8);
+            Console.WriteLine("s : Извлечение корня");
+
+            Console.SetCursorPosition(Console.WindowWidth - 25, 9);
+            Console.WriteLine("! : Факториал");
         }
     }
 }
